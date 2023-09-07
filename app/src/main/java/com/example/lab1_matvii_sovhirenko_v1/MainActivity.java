de2fa01226581f84fe74ff8d70fef0402c8da973
matvii <matveysovh@gmail.com>
2023-09-07 18:58:32 +0300

    Were added textView and button elements. Button was programmed to change textView content.

--- app/src/main/java/com/example/lab1_matvii_sovhirenko_v1/MainActivity.java
+++ app/src/main/java/com/example/lab1_matvii_sovhirenko_v1/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.lab1_matvii_sovhirenko_v1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvSecond;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvSecond = findViewById(R.id.tvSecondary);
+    }
+
+    public void buttonClick1(View v)
+    {
+        tvSecond.setText("THE TEXT WAS CHANGED SUCCESSFULLY");
     }
 }
